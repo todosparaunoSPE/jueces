7b6527e4901059d539a9684629615672fe181e8b
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-11-05 12:26:01 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -11,6 +11,23 @@ import pandas as pd
 import spacy
 import random
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
 # Cargar el modelo de spaCy
 nlp = spacy.load('en_core_web_sm')
 

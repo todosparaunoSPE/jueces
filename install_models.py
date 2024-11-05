7088672bef1fedd0fccb61ed094f9b54c4e48c2e
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-11-05 13:52:49 -0600

    Update install_models.py

--- install_models.py
+++ install_models.py
@@ -1,8 +1,4 @@
 import spacy
 
-try:
-    spacy.load('en_core_web_sm')
-except OSError:
-    # Si el modelo no se encuentra, lo descargamos
-    from spacy.cli import download
-    download('en_core_web_sm')
+# Instala el modelo de spaCy
+spacy.cli.download("en_core_web_sm")

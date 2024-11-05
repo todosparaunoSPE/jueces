86bfba2a56f7a0221a3a21e50db37c4376d91623
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-11-05 13:58:44 -0600

    Update install_models.py

--- install_models.py
+++ install_models.py
@@ -1,4 +1,13 @@
+import subprocess
 import spacy
 
-# Instala el modelo de spaCy
-spacy.cli.download("en_core_web_sm")
+# Intentar cargar el modelo, si no está disponible, instalarlo
+try:
+    nlp = spacy.load('en_core_web_sm')
+except OSError:
+    # Si el modelo no está instalado, descargarlo
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    # Luego cargar el modelo después de instalarlo
+    nlp = spacy.load('en_core_web_sm')
+
+# Ahora ya puedes usar 'nlp' para procesar texto
